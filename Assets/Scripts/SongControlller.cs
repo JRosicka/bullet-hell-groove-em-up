5b4c5e42812757f34e5cecc1513d4fb73878273c
Joseph Rosicka <JRosicka@bitbucket.org>
2020-06-26 14:51:24 -0500

    Adding song by section, making it play in the main scene

---
+++ Assets/Scripts/SongControlller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SongControlller : MonoBehaviour {
+    // Sound that's currently playing. Only one at a time, lads.
+    private AudioSource currentlyPlayingBackground;
+    public bool soundsToggled = true;
+    public AudioSource Music;
+
+    void Start() {
+        PlaySong(Music);
+    }
+
+    private void PlaySong(AudioSource sound) {
+        if (!soundsToggled)
+            return;
+
+        if (currentlyPlayingBackground)
+            currentlyPlayingBackground.Stop();
+
+        if (sound == null)
+            return;
+
+        currentlyPlayingBackground = sound;
+        sound.loop = false;
+        sound.Play();
+    }
+}
