12d80140dff196738059ae0f426372a8eb56f8a7
Joseph Rosicka <JRosicka@bitbucket.org>
2020-06-29 22:15:29 -0500

    Enabling particle systems to have their seeds be synchronizable - this is how we will be able to make bullet firing effects that have the same movement vectors as the bullets

---
+++ Assets/Scripts/SetParticleSeed.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class SetParticleSeed : MonoBehaviour
+{
+
+    public ParticleSystem[] setSeedParticles;
+    public int ParticleSeed;
+    public bool forceNewSeed;
+
+    void Start()
+    {
+        if (setSeedParticles != null && setSeedParticles.Length > 0)
+        {
+            for (int i = 0; i < setSeedParticles.Length; i++)
+            {
+                setSeedParticles[i].randomSeed = (uint)ParticleSeed;
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (forceNewSeed)
+        {
+            ParticleSeed = Random.Range(0, int.MaxValue);
+
+            if (setSeedParticles.Length > 0)
+            {
+                for (int i = 0; i < setSeedParticles.Length; i++)
+                {
+                    setSeedParticles[i].randomSeed = (uint)ParticleSeed;
+                }
+            }
+
+            forceNewSeed = false;
+        }
+    }
+}
---
+++ Assets/Scripts/SyncParticleSeeds.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class SyncParticleSeeds : MonoBehaviour
+{
+
+    public ParticleSystem[] setSeedParticles;
+    private int particleSeed;
+
+    void Start()
+    {
+        particleSeed = Random.Range(0, int.MaxValue);
+
+        if (setSeedParticles != null && setSeedParticles.Length > 0)
+        {
+            for (int i = 0; i < setSeedParticles.Length; i++)
+            {
+                setSeedParticles[i].randomSeed = (uint)particleSeed;
+            }
+        }
+    }
+}
