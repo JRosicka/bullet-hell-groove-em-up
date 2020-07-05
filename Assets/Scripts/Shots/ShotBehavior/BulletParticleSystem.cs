a9b96f8040618b5497b1648da2ce6866afc052bf
Joseph Rosicka <JRosicka@bitbucket.org>
2020-07-05 15:57:14 -0500

    Reorganizing file structure; reconfiguring; adding foundation for bullet particle systems

---
+++ Assets/Scripts/Shots/ShotBehavior/BulletParticleSystem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletParticleSystem : MonoBehaviour {
+    public ParticleSystem FireEffectOutline;
+    public ParticleSystem FireEffectMiddle;
+    public ParticleSystem VisibleOutline;
+    public ParticleSystem VisibleMiddle;
+
+    private uint particleSeed;
+
+    private void syncParticleSystems() {
+        syncParticleSeeds();
+
+        //FireEffectOutline.emission.
+    }
+
+    private void syncParticleSeeds() {
+        particleSeed = (uint)Random.Range(0, int.MaxValue);
+
+        FireEffectOutline.randomSeed = particleSeed;
+        FireEffectMiddle.randomSeed = particleSeed;
+        VisibleOutline.randomSeed = particleSeed;
+        VisibleMiddle.randomSeed = particleSeed;
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        syncParticleSystems();
+    }
+}
