e3314608978449a9e5fdbe643e1d922e9892608a
Joseph Rosicka <JRosicka@bitbucket.org>
2020-07-09 22:48:08 -0500

    Setting up control mapping for joystick and keyboard

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using Rewired;
+using UnityEngine;
 
 /// <summary>
 /// Handles the movement of the player, ensuring it remains inside the game boundary
@@ -8,7 +9,14 @@ public class Player : MonoBehaviour {
 	//public BoundaryEdges boundary;
 	public float normalSpeed;
 	public float slowSpeed;
-	
+
+
+	private const string HORIZONTAL_MOVEMENT_NAME = "ctr_move_horizontal";
+	private const string VERTICAL_MOVEMENT_NAME = "ctr_move_vertical";
+	private const string SLOW_NAME = "ctr_slow";
+	private const string PAUSE_NAME = "ctr_pause";
+	private const string SHOOT_NAME = "ctr_shoot";
+
 	void Start() {
 		//boundary = GameObject.Find("Boundary").GetComponent<BoundaryEdges>();
 	}
@@ -18,16 +26,37 @@ public class Player : MonoBehaviour {
 	/// as long as it remains within the game bounds
 	/// </summary>
 	void FixedUpdate() {
-		float moveHorizontal = Input.GetAxis("Horizontal");
-		float moveVertical = Input.GetAxis("Vertical");
+		// float moveHorizontal = Input.GetAxis("Horizontal");
+		// float moveVertical = Input.GetAxis("Vertical");
+		Rewired.Player playerControls = ReInput.players.GetPlayer("SYSTEM");
 
+		if(playerControls.controllers.joystickCount == 0) {
+			Joystick joystick = ReInput.controllers.GetJoystick(0);
+			playerControls.controllers.AddController(joystick, true);
+		}
+		
+		
+		float moveHorizontal = playerControls.GetAxis(HORIZONTAL_MOVEMENT_NAME);
+		float moveVertical = playerControls.GetAxis(VERTICAL_MOVEMENT_NAME);
+		// playerControls.controllers.Joysticks[0]
+
+		float currentSpeed = normalSpeed;
+		if (playerControls.GetButton(SLOW_NAME))
+			currentSpeed = slowSpeed;
+
+		if (playerControls.GetButton(PAUSE_NAME))
+			Debug.Log("PAAAAAAAUSE");
+		
+		if (playerControls.GetButton(SHOOT_NAME))
+			Debug.Log("SHOOOOT");
+			
 //		rb.position = new Vector2(
 //			Mathf.Clamp(rb.position.x, boundary.xMinWindow, boundary.xMaxWindow),
 //			Mathf.Clamp(rb.position.y, boundary.yMinWindow, boundary.yMaxWindow)
 //		);
 
 		Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-		float currentSpeed = normalSpeed;
+		
 		rb.velocity = movement * currentSpeed;
 	}
 }
