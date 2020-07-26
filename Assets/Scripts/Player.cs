329d9765e00f280ed8e2585fabc20ea90e06268f
Joseph Rosicka <JRosicka@bitbucket.org>
2020-07-26 18:40:06 -0500

    Making it so the player cannot move outside the game bounds lol

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,6 +11,16 @@ public class Player : MonoBehaviour {
 	public float normalSpeed;
 	public float slowSpeed;
 
+	public GameObject WallLeft;
+	public GameObject WallRight;
+	public GameObject WallUp;
+	public GameObject WallDown;
+
+	private bool onWallLeft;
+	private bool onWallRight;
+	private bool onWallUp;
+	private bool onWallDown;
+
 
 	private const string HORIZONTAL_MOVEMENT_NAME = "ctr_move_horizontal";
 	private const string VERTICAL_MOVEMENT_NAME = "ctr_move_vertical";
@@ -38,7 +48,16 @@ public class Player : MonoBehaviour {
 		
 		
 		float moveHorizontal = playerControls.GetAxis(HORIZONTAL_MOVEMENT_NAME);
+		if (onWallLeft)
+			moveHorizontal = Mathf.Clamp(moveHorizontal, 0, 1);
+		else if (onWallRight)
+			moveHorizontal = Mathf.Clamp(moveHorizontal, -1, 0);
+
 		float moveVertical = playerControls.GetAxis(VERTICAL_MOVEMENT_NAME);
+		if (onWallUp)
+			moveVertical = Mathf.Clamp(moveVertical, -1, 0);
+		else if (onWallDown)
+			moveVertical = Mathf.Clamp(moveVertical, 0, 1);
 		// playerControls.controllers.Joysticks[0]
 
 		float currentSpeed = normalSpeed;
@@ -59,9 +78,26 @@ public class Player : MonoBehaviour {
 		Vector2 movement = new Vector2(moveHorizontal, moveVertical);
 		
 		rb.velocity = movement * currentSpeed;
+		onWallLeft = false;
+		onWallRight = false;
+		onWallUp = false;
+		onWallDown = false;
+
 	}
 
 	private void OnParticleCollision(GameObject other) {
 		Debug.Log("Oof! Ouch!");
+	}
+
+	private void OnCollisionEnter2D(Collision2D other) {
+		if (other.gameObject == WallLeft) {
+			onWallLeft = true;
+		} else if (other.gameObject == WallRight) {
+			onWallRight = true;
+		} else if (other.gameObject == WallUp) {
+			onWallUp = true;
+		} else if (other.gameObject == WallDown) {
+			onWallDown = true;
+		}
 	}
 }
