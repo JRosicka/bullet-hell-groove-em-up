b99a947de8b4c22c9805e4e93a9aa44d62fd3df3
Joseph Rosicka <jrosicka@github.com>
2021-01-17 18:20:57 -0600

    Adding editor button tool

---
+++ Assets/Scripts/EditorButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EditorButton : MonoBehaviour {
+    public UnityEvent Event;
+
+#if UNITY_EDITOR
+    [CustomEditor(typeof(EditorButton))]
+    public class EmitterEditor : Editor {
+        public override void OnInspectorGUI() {
+            DrawDefaultInspector();
+            EditorButton button = target as EditorButton;
+            if (GUILayout.Button("Do the thing")) {
+                button.Event.Invoke();
+            }
+        }
+    }
+#endif
+
+}
