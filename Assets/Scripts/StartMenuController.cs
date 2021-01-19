7829e465655e4804044eabee2952af2c34c0e5bf
Joseph Rosicka <jrosicka@github.com>
2021-01-19 01:15:59 -0600

    Adding a basic start menu

---
+++ Assets/Scripts/StartMenuController.cs
@@ -0,0 +1,19 @@
+﻿using Rewired;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuController : MonoBehaviour {
+    private const string SLOW_NAME = "ctr_slow";
+
+    void Update() {
+        Rewired.Player playerControls = ReInput.players.GetPlayer("SYSTEM");
+
+        if (playerControls.controllers.joystickCount == 0) {
+            Joystick joystick = ReInput.controllers.GetJoystick(0);
+            playerControls.controllers.AddController(joystick, true);
+        }
+
+        if (playerControls.GetButton(SLOW_NAME))
+            SceneManager.LoadScene("MainScene");
+    }
+}
