fe51f3f9df180f613490e8a041764f00ad08ddaf
Joseph Rosicka <jrosicka@github.com>
2021-06-06 19:52:50 -0500

    Adding beginning of game background logic

---
+++ Assets/Scripts/GameBackground.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Represents a collection of background sprites and textures and stuff that can move around in a coordinated manner
+/// </summary>
+public class GameBackground : MonoBehaviour {
+    private const string BACKGROUND_START_ANIMATION_NAME = "background_start";
+    
+    // List of every GameObject that is a part of this background
+    public List<GameObject> BackgroundElements;
+
+    public List<Animator> Animators;
+    
+    public void Activate() {
+        foreach (GameObject element in BackgroundElements) {
+            element.SetActive(true);
+        }
+
+        foreach (Animator animator in Animators) {
+            animator.Play(BACKGROUND_START_ANIMATION_NAME);
+        }
+    }
+
+    public void Deactivate() {
+        foreach (Animator animator in Animators) {
+            animator.StopPlayback();
+        }
+
+        foreach (GameObject element in BackgroundElements) {
+            element.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/GameBackgroundController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+/// <summary>
+/// Keeps track of and controls transition between <see cref="GameBackground"/>s. 
+/// </summary>
+public class GameBackgroundController : MonoBehaviour {
+    public List<GameBackground> Backgrounds;
+    private GameBackground activeBackground;
+
+    public void TransitionToBackground(GameBackground destinationBackground) {
+        if (destinationBackground != null && activeBackground == destinationBackground)
+            return;
+
+        // Turn off the current background
+        activeBackground.Deactivate();
+        
+        // Turn on the new background
+        activeBackground = destinationBackground;
+        activeBackground.Activate();
+    }
+
+#if UNITY_EDITOR
+    [CustomEditor(typeof(GameBackgroundController))]
+    public class GameBackgroundControllerEditor : Editor {
+        public override void OnInspectorGUI() {
+            DrawDefaultInspector();
+            GameBackgroundController controller = target as GameBackgroundController;
+
+            if (controller == null)
+                return;
+
+            int i = 0;
+            foreach (GameBackground background in controller.Backgrounds) {
+                if (GUILayout.Button("Transition to BG " + i))
+                    controller.TransitionToBackground(background);
+                i++;
+            }
+        }
+    }
+#endif
+}
