10bfe5e0ce83d2b4e0dfdc6935f3646956d3bc17
Joseph Rosicka <jrosicka@github.com>
2022-01-03 19:51:57 -0600

    Allowing for more than two copies of the tile for vertical background scrollers

--- Assets/Scripts/Backgrounds/VerticalBackgroundScroller.cs
+++ Assets/Scripts/Backgrounds/VerticalBackgroundScroller.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Linq;
-using System.Numerics;
+﻿using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
-using Quaternion = UnityEngine.Quaternion;
 using Vector3 = UnityEngine.Vector3;
 
 /// <summary>
@@ -12,6 +9,8 @@ using Vector3 = UnityEngine.Vector3;
 [RequireComponent(typeof(VerticalLayoutGroup))]
 public class VerticalBackgroundScroller : BackgroundScroller {
     public bool ScrollBottomToTop;
+    [Range(2,15)]
+    public int NumberOfTileRepeats = 2;
     
     // The height of a single instance of the background tile - after traveling this distance, the 
     // background resets
@@ -20,14 +19,19 @@ public class VerticalBackgroundScroller : BackgroundScroller {
     private float timeOffset;
 
     public override void Initialize() {
-        // Make a clone of the tile object which gets picked up by the layout group - creates the infinite scroll effect
-        Instantiate(BackgroundTile, transform);
+        for (int i = 1; i < NumberOfTileRepeats; i++) {
+            // Make a clone of the tile object which gets picked up by the layout group - creates the infinite scroll effect
+            Instantiate(BackgroundTile, transform);
+        }
+
         backgroundHeight = BackgroundTile.GetComponent<RectTransform>().rect.height;
         if (!ScrollBottomToTop) {
             float angle = Mathf.Deg2Rad * transform.localEulerAngles.z + Mathf.PI / 2;
 
             // We need to move the whole thing up so that the cloned tile appears at the bottom
-            transform.position += new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0) * backgroundHeight;
+            transform.position += new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0) 
+                                    * backgroundHeight
+                                    * (NumberOfTileRepeats - 1);
         }
         startPosition = transform.position;
         backgroundElements = transform.GetComponentsInChildren<IBackgroundElement>().ToList();
