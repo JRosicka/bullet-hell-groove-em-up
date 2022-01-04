36ae453c50deb533804c905bc59315f30981a8b8
Joseph Rosicka <jrosicka@github.com>
2022-01-03 18:43:57 -0600

    Background 3 assets and creation; allowing vertical scrolling backgrounds to scroll at an angle

--- Assets/Scripts/Backgrounds/VerticalBackgroundScroller.cs
+++ Assets/Scripts/Backgrounds/VerticalBackgroundScroller.cs
@@ -1,6 +1,10 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Numerics;
 using UnityEngine;
 using UnityEngine.UI;
+using Quaternion = UnityEngine.Quaternion;
+using Vector3 = UnityEngine.Vector3;
 
 /// <summary>
 /// Scrolls a background across its y axis. Really, we are just resetting the position every so often. 
@@ -20,8 +24,10 @@ public class VerticalBackgroundScroller : BackgroundScroller {
         Instantiate(BackgroundTile, transform);
         backgroundHeight = BackgroundTile.GetComponent<RectTransform>().rect.height;
         if (!ScrollBottomToTop) {
+            float angle = Mathf.Deg2Rad * transform.localEulerAngles.z + Mathf.PI / 2;
+
             // We need to move the whole thing up so that the cloned tile appears at the bottom
-            transform.position += new Vector3(0, backgroundHeight, 0);
+            transform.position += new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0) * backgroundHeight;
         }
         startPosition = transform.position;
         backgroundElements = transform.GetComponentsInChildren<IBackgroundElement>().ToList();
@@ -30,7 +36,9 @@ public class VerticalBackgroundScroller : BackgroundScroller {
     private void Update() {
         float newPosition = Mathf.Repeat((Time.time + timeOffset) * ScrollSpeed, backgroundHeight) 
                             * (ScrollBottomToTop ? 1 : -1);
-        transform.position = startPosition + Vector3.up * newPosition;
+        float angle = Mathf.Deg2Rad * transform.localEulerAngles.z + Mathf.PI / 2;
+        Vector3 vector = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle)).normalized;
+        transform.position = startPosition + vector * newPosition;
     }
     
     public override void ResetBackground() {
